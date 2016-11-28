2b13d855dac08d74e4be2001da7fc3964a00c2db
Austin Hollenbaugh <austinhollenbaugh@gmail.com>
2016-11-28 11:37:31 -0700

    triathlon problem

--- codewars.js
+++ codewars.js
@@ -1,3 +1,40 @@
+//An Ironman Triathlon is one of a series of long-distance triathlon races organized by the World Triathlon Corporaion (WTC). It consists of a 2.4-mile swim, a 112-mile bicycle ride and a marathon (26.2-mile) (run, raced in that order and without a break. It hurts... trust me.
+
+// Your task is to take a distance that an athlete is through the race, and return one of the following:
+//
+// If the distance is zero, return 'Starting Line... Good Luck!'
+//
+// If the athlete will be swimming, return an object with 'Swim' as the key, and the remaining race distance as the value.
+//
+// If the athlete will be riding their bike, return an object with 'Bike' as the key, and the remaining race distance as the value.
+//
+// If the athlete will be running, and has more than 10 miles to go, return an object with 'Run' as the key, and the remaining race distance as the value.
+//
+// If the athlete has 10 miles or less to go, return return an object with 'Run' as the key, and 'Nearly there!' as the value.
+//
+// Finally, if the athlete has completed te distance, return "You're done! Stop running!"
+//
+// All distance should be calculated to two decimal places.
+
+function iTri(s) {
+  var distanceLeft = (140.60 - s).toFixed(2);
+  if (s === 0) {
+    return 'Starting Line... Good Luck!';
+  } else if (s > 0 && s <= 2.4) {
+    return {Swim: distanceLeft + ' to go!'};
+  } else if (s > 2.4 && s <= 114.4) {
+    return {Bike: distanceLeft + ' to go!'};
+  } else if (s > 114.4 && s < 130.6) {
+    return {Run: distanceLeft + ' to go!'};
+  } else if (s >= 130.6 && s < 140.6) {
+    return {Run: 'Nearly there!'};
+  } else if (s === 140.6) {
+    return "You're done! Stop running!";
+  } else
+};
+
+
+
 // Program an algorithm in your selected language that performs exactly 1 complete pass (no more, no less :D) of the Bubblesort algorithm to a list of positive integers. You may assume all arguments provided are arrays containing at least 2 positive integers.
 function bubblesortOnce(intArr) {
   for (var i = 0; i < intArr.length; i++) {
@@ -46,8 +83,9 @@ function weatherInfo (temp) {
   var c = convertToCelsius(temp);
   if (c <= 0)
     return (c + " is freezing temperature");
-  else
-    return (c + " is above freezing temperature");
+  else {
+    return c + " is above freezing temperature";
+  }
 }
 
 function convertToCelsius (temperature) {
@@ -62,4 +100,4 @@ function convertToCelsius (temperature) {
 
 var stringToNumber = function(str){
   return parseInt(str);
-}
+};

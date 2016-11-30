6ee90f384d591285528a893f9d4a7ad7c89e534f
Austin Hollenbaugh <austinhollenbaugh@gmail.com>
2016-11-29 23:57:33 -0700

    new problem

--- codewars.js
+++ codewars.js
@@ -1,3 +1,18 @@
+// Convert number to reversed array of digits
+//
+// Given a random number:
+//
+// C#: long;
+// C++: unsigned long;
+// You have to return the digits of this number within an array in reverse order.
+// Example:
+//
+// 348597 => [7,9,5,8,4,3]
+
+function digitize(n) {
+  return n.toString().split("").map(Number).reverse();
+}
+
 //Dan likes to find the nth power of number. But he doesn't like it when the result is odd!
 
 // Return the nth power of each number, unless the result is odd, in which case round the result to the nearest 10.
